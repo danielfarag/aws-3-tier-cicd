6e6032cb1910c0754450bba3c786e0688398b18b
DanielFarag <danielfarag146@gmail.com>
2025-05-25 17:58:45 +0300

    update frontent + backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -30,7 +30,7 @@ router.get('/:id', async (req, res) => {
   try {
     const post = await Post.findByPk(id);
     if (post) {
-      res.status(200).json(post); 
+      res.status(200).json({...post, detail: true}); 
     } else {
       res.status(404).json({ error: 'Post not found' });
     }
